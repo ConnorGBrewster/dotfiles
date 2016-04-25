ea3a796f2dcac7895b5591cbc9a79719bcfbb401
Connor Brewster <brewsterc@my.caspercollege.edu>
2016-04-24 23:03:19 -0600

    Added Vundle to install script

--- install.sh
+++ install.sh
@@ -19,5 +19,8 @@ chsh -s $(which zsh)
 source install/link.sh
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+nvim +PluginInstall +qall
 
 echo "Done."
