4830dd1d3791eb478055824448f5fdb14dc2fc86
Connor Brewster <brewsterc@my.caspercollege.edu>
2016-04-24 23:15:04 -0600

    Added extra install

--- install.sh
+++ install.sh
@@ -6,9 +6,14 @@ source install/link.sh
 
 if [ "$(uname)" == "Darwin" ]; then
     echo -e "\n\nRunning OSX specific scripts"
-    
     source install/brew.sh
 fi
+
+echo "Cloning Repositories"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 echo "Initializing Submodules"
 git submodule update --init --recursive
 
@@ -17,9 +22,6 @@ echo "Configuring zsh as default shell"
 chsh -s $(which zsh)
 
 source install/link.sh
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 nvim +PluginInstall +qall
 
